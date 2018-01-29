6f861bb21d71dad5129d8da0a32672c5e0f2d0ee
刘建林 <liujianlin@liujianlindeMacBook-Pro.local>
2018-01-29 15:53:53 +0800

    添加注释

--- src/config/routers.js
+++ src/config/routers.js
@@ -1,3 +1,12 @@
+/**
+ * 路由配置文件（按需加载）
+ * 配置格式
+ * {
+ *    path:"/example",
+ *    models:()=>[import('./models/example')]
+ *    component:()=>import('../router/example')
+ * }
+ */
 const routers = [
     {
         path:"/user",
